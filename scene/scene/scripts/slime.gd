d9cfd1c9410cbcfe862d9a31e553349c9eda30cb
RA-Salles <107194597+RA-Salles@users.noreply.github.com>
2023-02-10 15:58:02 -0300

    adsfasdf

---
+++ scene/scene/scripts/slime.gd
@@ -0,0 +1,161 @@
+extends KinematicBody2D
+
+
+"""
+PENDENCIAS
+	FUNÇÃO DE TOMAR DANO DO SLIME
+	IA NO INPUTGETTER -> deixar pra se por acaso sobrar tempo. 
+		Observação básica: não vai sobrar tempo
+	
+"""
+export(int) var speed = 10000
+export(bool) var is_ded = false
+export(int) var life
+var key: bool = false;
+var velocity = Vector2()
+var lastlook = Vector2()
+onready var animation = get_node("AnimationPlayer")
+onready var attackcollision = get_node("Area2D/CollisionShape2D")
+onready var sprite = get_node("Sprite")
+var didtheded = 0
+var is_attacking = false
+var attacks = ["down sword", "up sword", "right sword"]
+
+func _ready():
+	lastlook.x = 1
+	lastlook.y = 0
+	animation.play("right idle")
+	life = 3
+
+func inputgetter(): #programar aqui a questão da IA, se der tempo.
+	pass
+	
+
+func moveanimate():
+	if velocity.x == 0:
+		lastlook.x = 0
+		if velocity.y < 0:
+			animation.play("up run")
+			lastlook.y = -1
+		else:
+			animation.play("down run")
+			lastlook.y = 1
+	if velocity.y == 0:
+		lastlook.y = 0
+		if velocity.x > 0:
+			lastlook.x = 1
+			sprite.flip_h = false
+			animation.play("right run")
+		else:
+			lastlook.x = -1
+			sprite.flip_h = true
+			animation.play("right run")
+	if velocity.y != 0 and velocity.x != 0:
+		lastlook.y = 0
+		if velocity.x > 0:
+			lastlook.x = 1
+			sprite.flip_h = false
+			animation.play("right run")
+		else:
+			lastlook.x = -1
+			sprite.flip_h = true
+			animation.play("right run")
+
+			
+func idleanimate():
+	if lastlook.x == 0:
+		if lastlook.y > 0:
+			animation.play("down idle")
+		if lastlook.y < 0:
+			animation.play("up idle")
+	elif lastlook.y == 0:
+		if lastlook.x > 0:
+			sprite.flip_h = false
+			animation.play("right idle")
+		else:
+			lastlook.x = -1
+			sprite.flip_h = true
+			animation.play("right idle")
+
+func animate_attack():
+	if lastlook.x == 0:
+		if lastlook.y > 0:
+			animation.play("down sword")
+		if lastlook.y < 0:
+			animation.play("up sword")
+	elif lastlook.y == 0:
+		if lastlook.x > 0:
+			sprite.flip_h = false
+			animation.play("right sword")
+		else:
+			lastlook.x = -1
+			sprite.flip_h = true
+			animation.play("right sword")
+
+func colchanger():
+	if lastlook.x == 0:
+		if lastlook.y > 0:
+			attackcollision.position = Vector2(0, 24)
+			
+		if lastlook.y < 0:
+			attackcollision.position = Vector2(0, -7)
+			
+	elif lastlook.y == 0:
+		if lastlook.x > 0:
+			attackcollision.position = Vector2(16, 10)
+			
+		else:
+			lastlook.x = -1
+			attackcollision.position = Vector2(-16, 10)
+			
+
+func attackhandler():
+	"""
+	Lida com sistemas relacionados a atacar
+	"""
+	animate_attack()
+	colchanger()
+	
+	
+func _on_animation_finished(anim_name): #Looping animations make animations never end!
+	if anim_name == "die":
+		queue_free()
+	if anim_name in attacks:
+		is_attacking = false
+		
+func takedamage(damage):
+	life = life - damage
+
+func death_handler():
+	"""
+	A função realiza algumas atividades para lidar com a morte do jogador em conjunto com o else lá em baixo.
+	
+	1. toca a animação de morte
+	2. desabilita checagem de input (efetivamente matando o desgramado)
+	3. Troca sprite base para animação de dead idle
+	"""
+	if didtheded == 0:
+		animation.play("die")
+		didtheded = 1
+	
+func _physics_process(delta):
+	if is_ded == false:
+		if is_attacking == false:
+			inputgetter()
+			move_and_slide(velocity*delta)
+			if velocity.y != 0 or velocity.x != 0:
+				moveanimate()
+			else:
+				idleanimate()
+			if is_attacking == true:
+				attackhandler()
+			if life <= 0:
+				is_ded = true
+		else:
+			attackhandler()
+	elif is_ded == true: #Se estiver morto, he gets a fucking lights out :>
+		death_handler()
+		
+func getattacked(area):
+	if area.is_in_group("player_attack") :
+		get_tree().change_scene("res://scene/scenes/turncombat.tscn")
